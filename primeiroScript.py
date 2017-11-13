168cf8e58abb7e4d931e4d045634a187c044e401
RDY8799 <RDY8799@users.noreply.github.com>
2017-11-13 15:06:48 -0200

    Add files via upload

---
+++ primeiroScript.py
@@ -0,0 +1,13 @@
+print (' © RDY SOFTWARE | 2016 - 2017 All rights reserved ')
+
+name = input ('Qual o seu nome?')
+
+print ('Seja bem vindo' + name)
+
+print ('Vamos fazer algumas perguntas a você' + name + 'tá?!')
+
+idade = input ('Quantos anos você tem?')
+gosta = input ('Qual o nome da sua Pocotinha?')
+feliz = input ('Você gosta de lasanha?')
+
+print 'Você tem ' , idade , ', gosta muito da ' , gosta , ' e gosta de lasanha!'
